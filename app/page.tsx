304cde5b8a56f11a550b19fa3e7447d36e451ebd
Daniel Esquinazi <esquinazi.dani@gmail.com>
2025-05-17 16:40:20 -0300

    add clerk auth

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,39 @@
 import { db } from "@/server/db";
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 
 // There are other approaches to make this page dynamic, but this is the simplest one for the example.
 export const dynamic = 'force-dynamic';
 
-export default async function Home() {
-  const images = await (await db.query.images.findMany({ 
+export async function ImageGallery() {
+  const images = (await db.query.images.findMany({
     orderBy: (model, { desc }) => desc(model.id)
   }));
 
+  return <div className="flex flex-wrap gap-4">
+    {images.map((image) => (
+      <div key={image.id} className="size-50">
+        <img
+          src={image.url}
+          alt={`Image ${image.id}`}
+          className="object-cover rounded-[8px]"
+        />
+      </div>
+    ))}
+  </div>
+}
+
+export default async function Home() {
+
   return (
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <div className="flex flex-wrap gap-4">
-          {images.map((image) => (
-            <div key={image.id} className="size-50">
-                <img
-                  src={image.url}
-                  alt={`Image ${image.id}`}
-                  className="object-cover rounded-[8px]"
-                />
-            </div>
-          ))}
+    <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+      <SignedOut>
+        <div className="flex flex-col gap-4">
+          <h2 className="text-2xl">Please SignIn/Up to see the images</h2>
         </div>
-      </main>
+      </SignedOut>
+      <SignedIn>
+        <ImageGallery />
+      </SignedIn>
+    </main>
   );
 }
